97ad1657641fbd3c1966d36cdce37598a08a956a
AlbertWebs <albertmuhatia@gmail.com>
2024-07-31 13:10:58 +0300

    blueheights

---
+++ resources/views/services.blade.php
@@ -0,0 +1,67 @@
+{{--  --}}
+                <div class="col-lg-4 col-md-6 col-sm-12 gallery-block">
+                    <div class="gallery-block-one">
+                        <div class="inner-box">
+                            <div class="bg-layer" style="background-image: url('{{asset('theme/assets/images/gallery/gallery-7.jpg')}}');"></div>
+                            <h2><a href="{{url('/')}}">Online and Onsite Registration</a></h2>
+                            <div class="link"><a href="{{url('/')}}"><i class="icon-25"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 gallery-block">
+                    <div class="gallery-block-one">
+                        <div class="inner-box">
+                            <div class="bg-layer" style="background-image: url('{{asset('theme/assets/images/gallery/gallery-8.jpg')}}');"></div>
+                            <h2><a href="{{url('/')}}">Game drives</a></h2>
+                            <div class="link"><a href="{{url('/')}}"><i class="icon-25"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 gallery-block">
+                    <div class="gallery-block-one">
+                        <div class="inner-box">
+                            <div class="bg-layer" style="background-image: url('{{asset('theme/assets/images/gallery/gallery-9.jpg')}}');"></div>
+                            <h2><a href="{{url('/')}}">Audio Visual Equipment</a></h2>
+                            <div class="link"><a href="{{url('/')}}"><i class="icon-25"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 gallery-block">
+                    <div class="gallery-block-one">
+                        <div class="inner-box">
+                            <div class="bg-layer" style="background-image: url('{{asset('theme/assets/images/gallery/gallery-10.jpg')}}');"></div>
+                            <h2><a href="{{url('/')}}">Live Streaming</a></h2>
+                            <div class="link"><a href="{{url('/')}}"><i class="icon-25"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 gallery-block">
+                    <div class="gallery-block-one">
+                        <div class="inner-box">
+                            <div class="bg-layer" style="background-image: url('{{asset('theme/assets/images/gallery/gallery-11.jpg')}}');"></div>
+                            <h2><a href="{{url('/')}}">Designer Stands</a></h2>
+                            <div class="link"><a href="{{url('/')}}"><i class="icon-25"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-12 gallery-block">
+                    <div class="gallery-block-one">
+                        <div class="inner-box">
+                            <div class="bg-layer" style="background-image: url('{{asset('theme/assets/images/gallery/gallery-12.jpg')}}');"></div>
+                            <h2><a href="{{url('/')}}">Airport Transfers</a></h2>
+                            <div class="link"><a href="{{url('/')}}"><i class="icon-25"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-12 gallery-block">
+                    <div class="gallery-block-one">
+                        <div class="inner-box">
+                            <div class="bg-layer" style="background-image: url('{{asset('theme/assets/images/gallery/gallery-13.jpg')}}');"></div>
+                            <h2><a href="{{url('/')}}">Private parties</a></h2>
+                            <div class="link"><a href="{{url('/')}}"><i class="icon-25"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+                {{--  --}}
